24b9ebfba81fd02dfad3440716d8b60314b72a8d
Christian <ferchris82@hotmail.com>
2023-12-04 12:33:02 -0500

    Versión 0.1

--- src/Automovil.java
+++ src/Automovil.java
@@ -12,4 +12,18 @@ public class Automovil {
                 "\nauto.color = " + this.color +
                 "\nauto.cilindrada = " + this.cilindrada;
     }
+
+    public String acelerar(int rpm){
+        return "el auto " + fabricante + " acelerando a " + rpm + "rpm";
+    }
+
+    public String frenar(){
+        return this.fabricante + " " + modelo + " frenado!";
+    }
+
+    public String acelerarFrenar(int rpm){
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+    }
 }
