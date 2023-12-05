d22f91c13179982496ba3a24f08b214e74b5e95f
Christian <ferchris82@hotmail.com>
2023-12-05 11:42:17 -0500

    Versión 1.0

--- src/Automovil.java
+++ src/Automovil.java
@@ -6,52 +6,52 @@ public class Automovil {
     private double cilindrada;
     private int capacidadEstanque = 40;
 
-    public String leerFabricante(){
+    public String getFabricante() {
         return fabricante;
     }
 
-    public void asignarFabricante(String fabricante){
+    public void setFabricante(String fabricante) {
         this.fabricante = fabricante;
     }
 
-    public String leerModelo(){
+    public String getModelo() {
         return modelo;
     }
 
-    public void asignarModelo(String modelo){
+    public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
-    public String leerColor(){
+    public String getColor() {
         return color;
     }
 
-    public void asignarColor(String color){
+    public void setColor(String color) {
         this.color = color;
     }
 
-    public double leerCilindrada(){
+    public double getCilindrada() {
         return cilindrada;
     }
 
-    public void asignarCilindrada(double cilindrada){
+    public void setCilindrada(double cilindrada) {
         this.cilindrada = cilindrada;
     }
 
-    public int leerCapacidadEstanque(){
+    public int getCapacidadEstanque() {
         return capacidadEstanque;
     }
 
-    public void asignarCapacidadEstanque(int capacidadEstanque){
+    public void setCapacidadEstanque(int capacidadEstanque) {
         this.capacidadEstanque = capacidadEstanque;
     }
 
     //Funciones
     public String verDetalle(){
-        return "auto.fabricante = " + this.fabricante +
-                "\nauto.modelo = " + this.modelo +
-                "\nauto.color = " + this.color +
-                "\nauto.cilindrada = " + this.cilindrada;
+        return "auto.fabricante = " + this.getFabricante() +
+                "\nauto.modelo = " + this.getModelo() +
+                "\nauto.color = " + this.getColor() +
+                "\nauto.cilindrada = " + this.getCilindrada();
     }
 
     public String acelerar(int rpm){
